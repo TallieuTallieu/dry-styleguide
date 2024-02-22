886b6682061fda0c2f0413749c6a9554315d1f3b
Korneel Eeckhout <korneel@pienter.tech>
2024-02-22 09:25:14 +0100

    Copy files in root of type folders (e.g. atoms/_all.scss)

--- src/Console/AddCollection.php
+++ src/Console/AddCollection.php
@@ -16,17 +16,16 @@ class AddCollection extends Command
         return $signature
             ->setName('add-collection')
             ->setDescription('Add certain styleguide collection by git')
-            ->addArgument(Argument::create('repository')->setDescription('The repository (url) of the collection'))
-        ;
+            ->addArgument(Argument::create('repository')->setDescription('The repository (url) of the collection'));
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // Clone repository to Resources folder
-        shell_exec('git clone '.$input->getArgument('repository').' '.__DIR__.'/../Resources/collection');
+        shell_exec('git clone ' . $input->getArgument('repository') . ' ' . __DIR__ . '/../Resources/collection');
 
         // Sync files
-        $directories = Filesystem::directories(__DIR__.'/../Resources/collection');
+        $directories = Filesystem::directories(__DIR__ . '/../Resources/collection');
 
         foreach ($directories as $directory) {
 
@@ -34,38 +33,49 @@ class AddCollection extends Command
 
                 foreach (Filesystem::files($d) as $file) {
 
-                    $filePath = 'app/templates/styleguide/'.basename($directory).'/'.basename($d).'/'.basename($file);
+                    $filePath = 'app/templates/styleguide/' . basename($directory) . '/' . basename($d) . '/' . basename($file);
 
-                    if (! Filesystem::exists($filePath)) {
-                        $output->writeLine('move: '.$filePath);
+                    if (!Filesystem::exists($filePath)) {
+                        $output->writeLine('move: ' . $filePath);
                         Filesystem::move($file, $filePath);
                     }
                 }
             }
+
+            $rootFiles = Filesystem::files($directory);
+
+            foreach ($rootFiles as $rootFile) {
+                $rootFilePath = 'app/templates/styleguide/' . basename($directory) . '/' . basename($rootFile);
+                if (!Filesystem::exists($rootFilePath)) {
+                    $output->writeLine('move: ' . $rootFilePath);
+                    Filesystem::move($rootFile, $rootFilePath);
+                }
+            }
         }
+
 
         // Append styles/js
         $stylePath = 'style/sass/style.scss';
         $jsPath = 'js/app.js';
 
-        if (! $this->stringExistsInFile('../../app/templates/styleguide/atoms/**/*', $stylePath)) {
-            $output->writeLine('append styles to : '.$stylePath);
-            Filesystem::append($stylePath, PHP_EOL."@import '../../app/templates/styleguide/atoms/all';");
-            Filesystem::append($stylePath, PHP_EOL."@import '../../app/templates/styleguide/molecules/all';");
-            Filesystem::append($stylePath, PHP_EOL."@import '../../app/templates/styleguide/organisms/all';");
+        if (!$this->stringExistsInFile('../../app/templates/styleguide/atoms/**/*', $stylePath)) {
+            $output->writeLine('append styles to : ' . $stylePath);
+            Filesystem::append($stylePath, PHP_EOL . "@import '../../app/templates/styleguide/atoms/all';");
+            Filesystem::append($stylePath, PHP_EOL . "@import '../../app/templates/styleguide/molecules/all';");
+            Filesystem::append($stylePath, PHP_EOL . "@import '../../app/templates/styleguide/organisms/all';");
         }
 
-        if (! $this->stringExistsInFile('../app/templates/styleguide/**/*.js', $jsPath)) {
-            $output->writeLine('append js to : '.$jsPath);
-            Filesystem::append($jsPath, PHP_EOL."import '../app/templates/styleguide/**/*.js';");
+        if (!$this->stringExistsInFile('../app/templates/styleguide/**/*.js', $jsPath)) {
+            $output->writeLine('append js to : ' . $jsPath);
+            Filesystem::append($jsPath, PHP_EOL . "import '../app/templates/styleguide/**/*.js';");
         }
 
         // Remove collection folder
-        shell_exec('rm -rf '.__DIR__.'/../Resources/collection');
+        shell_exec('rm -rf ' . __DIR__ . '/../Resources/collection');
     }
 
     private function stringExistsInFile(string $searchFor, $file)
     {
-       return strpos(Filesystem::get($file), $searchFor);
+        return strpos(Filesystem::get($file), $searchFor);
     }
 }
